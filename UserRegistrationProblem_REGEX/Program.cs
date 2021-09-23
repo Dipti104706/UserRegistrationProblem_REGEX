6039c6ac19ef87f643d44f03411b2da4532d592a
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-23 17:17:21 +0530

    [Dipti] added usercase 1 to validating first name in user registration

---
+++ UserRegistrationProblem_REGEX/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem_REGEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration user = new UserRegistration();
+            user.Validation();
+            Console.ReadLine();
+        }
+    }
+}
