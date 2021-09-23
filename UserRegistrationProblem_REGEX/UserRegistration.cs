45d7222a4147a3f1951fd9709b399742d4b3b58d
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-23 19:03:28 +0530

    [Dipti] added usercase 7 to validating password with at least o1 number in user registration

--- UserRegistrationProblem_REGEX/UserRegistration.cs
+++ UserRegistrationProblem_REGEX/UserRegistration.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace UserRegistrationProblem_REGEX
 {/// <summary>
-/// UC6 creating regular expression for validating password with at least one uppercase
+/// UC7 creating regular expression for validating password with at least one numeric value
 /// </summary>
     class UserRegistration
     {
         //writing the valid pattern
-        string password = "^[a-zA-Z]{1,}[A-Z]{1,}[a-zA-Z]*";
-        string[] inputs = {"sdAcfghj","asd","AASDEFFAgkkhkhu"};
+        string password = "^[a-zA-Z]*[A-Z]*[0-9]+[a-zA-Z]*$";
+        string[] inputs = {"sdAcf1ghj","asd","AASDEFFAgkkhkhu"};
 
         /// <summary>
         /// Validation this instance.
